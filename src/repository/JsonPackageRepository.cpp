2f8e72c8a832e7383565d1a8f7f07ec04b225322
Khang Do <khangdminh07@gmail.com>
2026-07-14 09:30:05 +0700

    Revert "feat: add name field for PackageMetadata"
    This reverts commit 998bde88a46aa7e14a0ef3ef38ea8391249b2155.

--- src/repository/JsonPackageRepository.cpp
+++ src/repository/JsonPackageRepository.cpp
@@ -18,13 +18,6 @@
  * @date   2026-06-24
  * @changelog
  *   - Change packageFromJson() to using load() instead of constructor
- * 
- * @update
- * @author Do Minh Khang
- * @date   2026-07-11
- * @changelog
- *   - Add package name to related functions: metadataToJson() and metadataFromJson()
- * 
  */
 
 #include "repository/JsonPackageRepository.h"
@@ -272,7 +265,6 @@ namespace wms::repository
         dim["height"] = m.dimensions.height;
 
         QJsonObject obj;
-        obj["name"] = QString::fromStdString(m.name);
         obj["category"] = categoryStr(m.category);
         obj["weight"] = m.weight;
         obj["cost"] = m.cost;
@@ -294,7 +286,6 @@ namespace wms::repository
 
         const QJsonObject dim = o["dimensions"].toObject();
         return domain::PackageMetadata{
-            o["name"].toString().toStdString(),
             categoryFromStr(o["category"].toString()),
             o["weight"].toDouble(),
             domain::Dimension{
@@ -342,14 +333,12 @@ namespace wms::repository
         const QStringList parts = s.split('-');
         if (parts.size() != 3)
             throw std::runtime_error(
-                "JsonPackageRepository::dateFromString - invalid date: " +
-                s.toStdString()
-            );
+                "JsonPackageRepository::dateFromString - invalid date: " + s.toStdString());
 
-        return domain::Date{
-            static_cast<int>(parts[0].toInt()),
-            static_cast<unsigned>(parts[1].toUInt()),
-            static_cast<unsigned>(parts[2].toUInt())
+        return std::chrono::year_month_day{
+            std::chrono::year  { parts[0].toInt() },
+            std::chrono::month { static_cast<unsigned>(parts[1].toUInt()) },
+            std::chrono::day   { static_cast<unsigned>(parts[2].toUInt()) }
         };
     }
 
