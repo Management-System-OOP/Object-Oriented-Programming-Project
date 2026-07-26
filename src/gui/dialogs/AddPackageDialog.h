e0a6c3fb1f59a550349d2fc4fe674c26d1ab5b3b
cyan-cpu <anhhaoduong2007@gmail.com>
2026-07-26 19:05:07 +0700

    Integrate QTabWidget for better UX in AddPackageDialog
    Added a QTabWidget to improve user experience by organizing form fields into tabs.

--- src/gui/dialogs/AddPackageDialog.h
+++ src/gui/dialogs/AddPackageDialog.h
@@ -3,12 +3,16 @@
  * @brief   Dialog for collecting full package fields and building a domain Package.
  * @author  Nguyen Viet Bach
  * @date    2026-06-24
- * 
- * @update
+ * * @update
  * @author  Lam Hong Hai Hoang Le
  * @date    2026-07-26
  * @changelog
- *   - Added support for name field in metadata
+ * - Added support for name field in metadata
+ * * @update
+ * @author  Duong Anh Hao
+ * @date    2026-07-26
+ * @changelog
+ * - Wrapped existing form fields into QTabWidget for better UX.
  */
 
 #pragma once
@@ -20,6 +24,7 @@
 #include <QSpinBox>
 #include <QDateEdit>
 #include <QDialogButtonBox>
+#include <QTabWidget> // Added for Tab UI
 
 #include "domain/entities/Package.h"
 
@@ -35,10 +40,10 @@ namespace wms::gui::dialogs {
         /**
          * @brief  Builds and returns a new Package from the current form values.
          *
-         *  Reads all input widgets, constructs the required value objects
-         *  (PackageMetadata, Address, LogisticsInfo, StorageLocation) and
-         *  delegates to Package::create() to produce a fully initialised
-         *  package with a generated UUID in OnRoute state.
+         * Reads all input widgets, constructs the required value objects
+         * (PackageMetadata, Address, LogisticsInfo, StorageLocation) and
+         * delegates to Package::create() to produce a fully initialised
+         * package with a generated UUID in OnRoute state.
          *
          * @return A new Package instance ready to be handed to WarehouseManager.
          * @throws std::invalid_argument if the export date is before the import date.
@@ -46,19 +51,21 @@ namespace wms::gui::dialogs {
         wms::domain::Package packageData() const;
 
     private:
-        QLineEdit*        m_nameEdit           { nullptr };
-        QLineEdit*        m_descriptionEdit    { nullptr };
-        QComboBox*        m_categoryCombo      { nullptr };
-        QDoubleSpinBox*   m_weightSpin         { nullptr };
-        QLineEdit*        m_zoneEdit           { nullptr };
-        QLineEdit*        m_aisleEdit          { nullptr };
-        QSpinBox*         m_shelfSpin          { nullptr };
-        QSpinBox*         m_slotSpin           { nullptr };
-        QLineEdit*        m_sourceCityEdit      { nullptr };
-        QLineEdit*        m_destinationCityEdit { nullptr };
-        QDateEdit*        m_importDateEdit     { nullptr };
-        QDateEdit*        m_exportDateEdit     { nullptr };
-        QDialogButtonBox* m_buttonBox          { nullptr };
+        QTabWidget* m_tabWidget{ nullptr }; // Added Tab Widget
+
+        QLineEdit* m_nameEdit{ nullptr };
+        QLineEdit* m_descriptionEdit{ nullptr };
+        QComboBox* m_categoryCombo{ nullptr };
+        QDoubleSpinBox* m_weightSpin{ nullptr };
+        QLineEdit* m_zoneEdit{ nullptr };
+        QLineEdit* m_aisleEdit{ nullptr };
+        QSpinBox* m_shelfSpin{ nullptr };
+        QSpinBox* m_slotSpin{ nullptr };
+        QLineEdit* m_sourceCityEdit{ nullptr };
+        QLineEdit* m_destinationCityEdit{ nullptr };
+        QDateEdit* m_importDateEdit{ nullptr };
+        QDateEdit* m_exportDateEdit{ nullptr };
+        QDialogButtonBox* m_buttonBox{ nullptr };
     };
 
 } // namespace wms::gui::dialogs
