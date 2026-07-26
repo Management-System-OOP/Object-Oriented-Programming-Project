dd1901f31a6361c4a06cf118a173600cf133f250
cyan-cpu <anhhaoduong2007@gmail.com>
2026-07-26 19:04:23 +0700

    Refactor EditPackageDialog to use QTabWidget
    Refactored EditPackageDialog to use QTabWidget for better organization of input fields, separating them into Metadata, Location, and Logistics tabs.

--- src/gui/dialogs/EditPackageDialog.cpp
+++ src/gui/dialogs/EditPackageDialog.cpp
@@ -1,7 +1,7 @@
 /**
  * @file    EditPackageDialog.cpp
  * @brief   Implementation of the dialog used to edit mutable fields of an
- *          existing Package (metadata, logistics, storage location).
+ * existing Package (metadata, logistics, storage location).
  * @author  Nguyen Viet Bach
  * @date    2026-06-24
  *
@@ -9,14 +9,19 @@
  * @author  Nguyen Viet Bach
  * @date    2026-07-04
  * @changelog
- *   - Added pre-populated form fields that load existing Package data and
- *     return updated value objects (metadata, logistics, location) on accept
- * 
- * @update
+ * - Added pre-populated form fields that load existing Package data and
+ * return updated value objects (metadata, logistics, location) on accept
+ * * @update
  * @author  Lam Hong Hai Hoang Le
  * @date    2026-07-26
  * @changelog
- *   - Added support for name field in metadata
+ * - Added support for name field in metadata
+ *
+ * @update
+ * @author  Duong Anh Hao
+ * @date    2026-07-26
+ * @changelog
+ * - Organized input form fields into QTabWidget
  */
 
 #include "EditPackageDialog.h"
@@ -70,8 +75,13 @@ namespace wms::gui::dialogs {
         setMinimumSize(480, 360);
 
         auto* mainLayout = new QVBoxLayout(this);
-        auto* formLayout = new QFormLayout();
-        
+
+        m_tabWidget = new QTabWidget(this);
+
+        // Metadata Tab
+        auto* metadataTab = new QWidget();
+        auto* metadataLayout = new QFormLayout(metadataTab);
+
         m_nameEdit = new QLineEdit(QString::fromStdString(package.metadata().name), this);
         m_nameEdit->setPlaceholderText("Package Name");
 
@@ -80,11 +90,11 @@ namespace wms::gui::dialogs {
 
         m_categoryCombo = new QComboBox(this);
         m_categoryCombo->addItem("Standard", static_cast<int>(wms::domain::Category::Standard));
-        m_categoryCombo->addItem("Fragile",  static_cast<int>(wms::domain::Category::Fragile));
+        m_categoryCombo->addItem("Fragile", static_cast<int>(wms::domain::Category::Fragile));
         m_categoryCombo->addItem("Perishable", static_cast<int>(wms::domain::Category::Perishable));
-        m_categoryCombo->addItem("Hazmat",   static_cast<int>(wms::domain::Category::Hazmat));
+        m_categoryCombo->addItem("Hazmat", static_cast<int>(wms::domain::Category::Hazmat));
         m_categoryCombo->addItem("Oversized", static_cast<int>(wms::domain::Category::Oversized));
-        m_categoryCombo->addItem("Liquid",   static_cast<int>(wms::domain::Category::Liquid));
+        m_categoryCombo->addItem("Liquid", static_cast<int>(wms::domain::Category::Liquid));
         m_categoryCombo->setCurrentIndex(categoryIndex(m_categoryCombo, package.metadata().category));
 
         m_weightSpin = new QDoubleSpinBox(this);
@@ -92,31 +102,45 @@ namespace wms::gui::dialogs {
         m_weightSpin->setValue(package.metadata().weight);
         m_weightSpin->setSuffix(" kg");
 
-        m_zoneEdit  = new QLineEdit(QString::fromStdString(package.location().zone),  this);
+        metadataLayout->addRow("Name:", m_nameEdit);
+        metadataLayout->addRow("Description:", m_descriptionEdit);
+        metadataLayout->addRow("Category:", m_categoryCombo);
+        metadataLayout->addRow("Weight:", m_weightSpin);
+        m_tabWidget->addTab(metadataTab, "Metadata");
+
+        // Location Tab
+        auto* locationTab = new QWidget();
+        auto* locationLayout = new QFormLayout(locationTab);
+
+        m_zoneEdit = new QLineEdit(QString::fromStdString(package.location().zone), this);
         m_aisleEdit = new QLineEdit(QString::fromStdString(package.location().aisle), this);
         m_shelfSpin = new QSpinBox(this);
         m_shelfSpin->setRange(1, 99);
         m_shelfSpin->setValue(package.location().shelf);
-        m_slotSpin  = new QSpinBox(this);
+        m_slotSpin = new QSpinBox(this);
         m_slotSpin->setRange(1, 99);
         m_slotSpin->setValue(package.location().slot);
+
+        locationLayout->addRow("Zone:", m_zoneEdit);
+        locationLayout->addRow("Aisle:", m_aisleEdit);
+        locationLayout->addRow("Shelf:", m_shelfSpin);
+        locationLayout->addRow("Slot:", m_slotSpin);
+        m_tabWidget->addTab(locationTab, "Location");
+
+        // Logistics Tab
+        auto* logisticsTab = new QWidget();
+        auto* logisticsLayout = new QFormLayout(logisticsTab);
 
         m_importDateEdit = new QDateEdit(qDateFromDomain(package.logistics().importDate), this);
         m_importDateEdit->setCalendarPopup(true);
         m_exportDateEdit = new QDateEdit(qDateFromDomain(package.logistics().expectedExportDate), this);
         m_exportDateEdit->setCalendarPopup(true);
 
-        formLayout->addRow("Name:",         m_nameEdit);
-        formLayout->addRow("Description:",  m_descriptionEdit);
-        formLayout->addRow("Category:",     m_categoryCombo);
-        formLayout->addRow("Weight:",       m_weightSpin);
-        formLayout->addRow("Zone:",         m_zoneEdit);
-        formLayout->addRow("Aisle:",        m_aisleEdit);
-        formLayout->addRow("Shelf:",        m_shelfSpin);
-        formLayout->addRow("Slot:",         m_slotSpin);
-        formLayout->addRow("Import Date:",  m_importDateEdit);
-        formLayout->addRow("Export Date:",  m_exportDateEdit);
-        mainLayout->addLayout(formLayout);
+        logisticsLayout->addRow("Import Date:", m_importDateEdit);
+        logisticsLayout->addRow("Export Date:", m_exportDateEdit);
+        m_tabWidget->addTab(logisticsTab, "Logistics");
+
+        mainLayout->addWidget(m_tabWidget);
 
         m_buttonBox = new QDialogButtonBox(QDialogButtonBox::Save | QDialogButtonBox::Cancel, this);
         mainLayout->addWidget(m_buttonBox);
@@ -130,9 +154,9 @@ namespace wms::gui::dialogs {
         wms::domain::Package updated = m_original;
 
         wms::domain::PackageMetadata metadata = updated.metadata();
-        metadata.name        = m_nameEdit->text().toStdString();
-        metadata.category    = static_cast<wms::domain::Category>(m_categoryCombo->currentData().toInt());
-        metadata.weight      = m_weightSpin->value();
+        metadata.name = m_nameEdit->text().toStdString();
+        metadata.category = static_cast<wms::domain::Category>(m_categoryCombo->currentData().toInt());
+        metadata.weight = m_weightSpin->value();
         metadata.description = m_descriptionEdit->text().toStdString();
         updated.setMetadata(std::move(metadata));
 
@@ -141,11 +165,11 @@ namespace wms::gui::dialogs {
             m_aisleEdit->text().toStdString(),
             m_shelfSpin->value(),
             m_slotSpin->value()
-        });
+            });
 
         wms::domain::LogisticsInfo logistics = updated.logistics();
-        logistics.importDate           = dateFromQDate(m_importDateEdit->date());
-        logistics.expectedExportDate   = dateFromQDate(m_exportDateEdit->date());
+        logistics.importDate = dateFromQDate(m_importDateEdit->date());
+        logistics.expectedExportDate = dateFromQDate(m_exportDateEdit->date());
         updated.setLogistics(std::move(logistics));
 
         return updated;
