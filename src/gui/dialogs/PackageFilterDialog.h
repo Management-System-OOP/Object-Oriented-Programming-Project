ee9552d8a3dc3e42910f0399a1908b8fa5f2da66
cyan-cpu <anhhaoduong2007@gmail.com>
2026-08-02 04:10:37 +0700

    Refactor PackageFilterDialog to add date filters

--- src/gui/dialogs/PackageFilterDialog.h
+++ src/gui/dialogs/PackageFilterDialog.h
@@ -1,75 +1,31 @@
-﻿/**
- * @file    PackageFilterDialog.h
- * @brief   Dialog for building a PackageQueryCriteria object based on user input.
- * @author  Duong Anh Hao
- * @date    2026-07-27
- * 
- * Update
- * @author Duong Anh Hao
- * @date    2026-07-29
- * @changelog
- * - Added more fields (Name, Description Keyword, Zone, Container ID).
- * 
- */
-
-#pragma once
-
-#include <QDialog>
-#include <QLineEdit>
-#include <QComboBox>
-#include <QDoubleSpinBox>
-#include <QCheckBox>
-#include <QDialogButtonBox>
-#include <QGroupBox>
-#include <QVBoxLayout>
-
-#include "domain/queries/PackageQueryCriteria.h"
-
-namespace wms::gui::dialogs {
-
-    class PackageFilterDialog : public QDialog
+void PackageFilterDialog::setupQuickTogglesGroup(QVBoxLayout* mainLayout)
     {
-        Q_OBJECT
+        auto* group = new QGroupBox("Date & Status Filters", this);
+        auto* layout = new QGridLayout(group);
+        layout->setHorizontalSpacing(10);
+        layout->setVerticalSpacing(10);
 
-    public:
-        explicit PackageFilterDialog(QWidget* parent = nullptr);
+        // 1. Import Filter
+        m_filterImportCheck = new QCheckBox("Filter by Import Date:", this);
+        m_importDateEdit = new QDateEdit(QDate::currentDate(), this);
+        m_importDateEdit->setCalendarPopup(true);
+        m_importDateEdit->setEnabled(false);
 
-        /**
-         * @brief Collects all input fields and generates the query criteria.
-         * Fields left empty or set to "Any" will remain std::nullopt.
-         * * @return wms::domain::PackageQueryCriteria containing the user's active filters.
-         */
-        wms::domain::PackageQueryCriteria getCriteria() const;
+        connect(m_filterImportCheck, &QCheckBox::toggled, m_importDateEdit, &QWidget::setEnabled);
 
-        /**
-         * @brief Resets all UI fields to their default (empty/unset) state.
-         */
-        void resetFilters();
+        layout->addWidget(m_filterImportCheck, 0, 0);
+        layout->addWidget(m_importDateEdit, 0, 1);
 
-    private:
+        // 2. Export Filter
+        m_filterExportCheck = new QCheckBox("Filter by Export Date:", this);
+        m_exportDateEdit = new QDateEdit(QDate::currentDate(), this);
+        m_exportDateEdit->setCalendarPopup(true);
+        m_exportDateEdit->setEnabled(false);
 
-        QLineEdit* m_nameEdit{ nullptr };
-        QLineEdit* m_zoneEdit{ nullptr };
-        QLineEdit* m_containerIdEdit{ nullptr };
-        QLineEdit* m_descriptionKeywordEdit{ nullptr };
+        connect(m_filterExportCheck, &QCheckBox::toggled, m_exportDateEdit, &QWidget::setEnabled);
 
-        // Classification & Status Fields
-        QComboBox* m_stateCombo{ nullptr };
-        QComboBox* m_categoryCombo{ nullptr };
-        QDoubleSpinBox* m_minWeightSpin{ nullptr };
-        QDoubleSpinBox* m_maxWeightSpin{ nullptr };
+        layout->addWidget(m_filterExportCheck, 1, 0);
+        layout->addWidget(m_exportDateEdit, 1, 1);
 
-        // Quick Toggle Fields (Booleans)
-        QCheckBox* m_overdueCheck{ nullptr };
-        QCheckBox* m_importedTodayCheck{ nullptr };
-        QCheckBox* m_exportDueTodayCheck{ nullptr };
-
-        QDialogButtonBox* m_buttonBox{ nullptr };
-
-        // Helper setup functions
-        void setupTextFiltersGroup(QVBoxLayout* mainLayout);
-        void setupClassificationGroup(QVBoxLayout* mainLayout);
-        void setupQuickTogglesGroup(QVBoxLayout* mainLayout);
-    };
-
-} // namespace wms::gui::dialogs
+        mainLayout->addWidget(group);
+    }
